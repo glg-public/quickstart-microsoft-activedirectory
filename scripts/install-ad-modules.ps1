ca6d4afc062de7ed6b6f9c45924671418af1e811
Mike Martin <mikemartin1090@gmail.com>
2020-05-07 00:27:24 -0400

    check if path is there to avoid errors in logs

--- scripts/install-ad-modules.ps1
+++ scripts/install-ad-modules.ps1
@@ -1,15 +1,13 @@
 [CmdletBinding()]
 param()
 
-#"Setting Execution Policy to Remote Signed"
-#Set-ExecutionPolicy RemoteSigned -Force
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 "Setting up Powershell Gallery to Install DSC Modules"
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5 -Force
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 
-"Installing the needed Powershell DSC modules for this Quick Start"
+"Installing the needed Powershell DSC modules"
 Install-Module NetworkingDsc
 Install-Module -Name "xActiveDirectory"
 Install-Module ComputerManagementDsc
@@ -20,11 +18,12 @@ Install-Module -Name "ActiveDirectoryCSDsc"
 Get-NetFirewallProfile | Set-NetFirewallProfile -Enabled False
 
 "Creating Directory for DSC Public Cert"
-New-Item -Path C:\AWSQuickstart\publickeys -ItemType directory 
+$CERTDIR = 'C:\AWSQuickstart\publickeys'
+if(!(Test-Path -Path $CERTDIR )){
+    New-Item -ItemType directory -Path $CERTDIR
+}
 
 "Setting up DSC Certificate to Encrypt Credentials in MOF File"
 $cert = New-SelfSignedCertificate -Type DocumentEncryptionCertLegacyCsp -DnsName 'AWSQSDscEncryptCert' -HashAlgorithm SHA256
 # Exporting the public key certificate
-$cert | Export-Certificate -FilePath "C:\AWSQuickstart\publickeys\AWSQSDscPublicKey.cer" -Force
-
-
+$cert | Export-Certificate -FilePath "$CERTDIR\AWSQSDscPublicKey.cer" -Force
